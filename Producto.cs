e00ddbbec3d71270f6bd463c9a6479d282424851
jeymiturcios <jeymiturcios13@gmail.com>
2026-01-28 22:13:25 -0600

    classs-producto

---
+++ Producto.cs
@@ -0,0 +1,8 @@
+public class Producto
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public decimal Precio { get; set; }
+    public int Stock { get; set; }
+    public string Descripcion { get; set; }
+}
